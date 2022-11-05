49fe1c39348c1c3bc3e0af12b7edce3c297937e8
Iury189 <iury.fernandes@protonmail.com>
2022-11-05 10:34:32 -0300

    Change CDN to local files.

--- resources/views/templates/template_hunter.blade.php
+++ resources/views/templates/template_hunter.blade.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="{{ url('css/bootstrap.css') }}"/>
-    <script type="text/javascript" href="{{ url('js/bootstrap.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://kit.fontawesome.com/41b4cd8ba8.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('jquery/jquery-3.6.1.slim.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('jquery/jquery.mask.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('jquery/popper.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('fontawesome/41b4cd8ba8.js') }}"></script>
     <title>@yield('title')</title>
 </head>
 <body>
